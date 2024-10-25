fbd96981c82cdc062f812af8335fb44ab1b62b5d
Ubiratan Neto <ubiratanneto37@gmail.com>
2024-10-25 15:57:28 -0300

    feat: add possibility to pass multiple labels

--- main.go
+++ main.go
@@ -101,7 +101,7 @@ func getLatestTag(label string) string {
 
 func yesNoPrompt(text string) bool {
 	prompt := promptui.Select{
-		Label: fmt.Sprintf("%s", text),
+		Label: text,
 		Items: []string{"no", "yes"},
 	}
 	_, result, err := prompt.Run()
@@ -136,71 +136,7 @@ func labelSelectionPrompt(allowedLabels []string) (string, bool) {
 	return result, result == "[cancel]"
 }
 
-func increment(label, inc, pre string, release bool, configFile string) {
-	// Get the config
-	config := readConfig(configFile)
-
-	// Check label restrictions
-	if config.AllowedLabels != nil {
-		// Check if label is required to exist, and then check if exists. If not, open a prompt to select
-		if len(label) == 0 {
-			var canceled bool
-			label, canceled = labelSelectionPrompt(config.AllowedLabels)
-			if canceled {
-				os.Exit(0)
-			}
-		}
-
-		// Check if label is included on allowed labels
-		if !slices.Contains(config.AllowedLabels, label) {
-			fmt.Printf("%sError: The label is not allowed to be used\n%s", COLOR_RED, COLOR_END)
-			os.Exit(1)
-		}
-	}
-
-	// Check if there are branch restrictions
-	if config.AllowedBranches != nil {
-		currentBranchCmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
-		currentBranch, err := currentBranchCmd.Output()
-
-		if err != nil {
-			fmt.Printf("%sError: unable to get current branch: %v\n%s", COLOR_RED, err, COLOR_END)
-			os.Exit(1)
-		}
-
-		if !slices.Contains(config.AllowedBranches, strings.TrimSpace(string(currentBranch))) {
-			fmt.Println(strings.TrimSpace(string(currentBranch)))
-			fmt.Printf("%sError: current branch is not allowed to be tagged\n%s", COLOR_RED, COLOR_END)
-			os.Exit(1)
-		}
-	}
-
-	// Check if Git working directory is clean
-	statusCmd := exec.Command("git", "status", "--porcelain")
-	statusOut, err := statusCmd.Output()
-
-	if err != nil {
-		fmt.Printf("%sError executing git command: %v\n%s", COLOR_RED, err, COLOR_END)
-		os.Exit(1)
-	}
-
-	if len(string(statusOut)) != 0 {
-		fmt.Printf("%sError: Git working directory not clean. Commit your changes before versioning.\n%s", COLOR_RED, COLOR_END)
-		os.Exit(1)
-	}
-
-	// Pull git changes and tags
-	pullCmd := exec.Command("git", "pull", "--tags")
-	_, err = pullCmd.Output()
-
-	if err != nil {
-		fmt.Printf("%sError executing git pull command: %v\n%s", COLOR_RED, err, COLOR_END)
-		os.Exit(1)
-	}
-
-	// Get the latest tag
-	latestTag := getLatestTag(label)
-
+func incrementTag(label, latestTag, inc, pre string, release bool) string {
 	// Check for valid increment mode
 	if len(inc) > 0 && !slices.Contains([]string{"major", "minor", "patch"}, inc) {
 		fmt.Printf("%sError: invalid value for increment mode\n%s", COLOR_RED, COLOR_END)
@@ -250,19 +186,112 @@ func increment(label, inc, pre string, release bool, configFile string) {
 	}
 	tag := fmt.Sprintf("%s%s", prefix, ver.String())
 
-	if !yesNoPrompt(fmt.Sprintf("Creating tag <%s>. Are you sure?", tag)) {
-		os.Exit(0)
+	return tag
+}
+
+func increment(labelStr, inc, pre string, release bool, configFile string) {
+	// Get the config
+	config := readConfig(configFile)
+
+	labels := strings.Split(labelStr, ",")
+
+	// remove empty labels
+	for i := 0; i < len(labels); i++ {
+		if len(labels[i]) == 0 {
+			labels = append(labels[:i], labels[i+1:]...)
+			i--
+		}
 	}
 
-	tagCmd := exec.Command("git", "tag", "-a", tag, "-m", fmt.Sprintf(`"%s"`, tag))
-	_, err = tagCmd.Output()
+	// Check label restrictions
+	if config.AllowedLabels != nil {
+		// Check if label is required to exist, and then check if exists. If not, open a prompt to select
+		if len(labels) == 0 {
+			var canceled bool
+			label, canceled := labelSelectionPrompt(config.AllowedLabels)
+			if canceled {
+				os.Exit(0)
+			}
+
+			labels = append(labels, label)
+		}
+
+		// Check if label is included on allowed labels
+		for _, label := range labels {
+			if !slices.Contains(config.AllowedLabels, label) {
+				fmt.Printf("%sError: The label is not allowed to be used\n%s", COLOR_RED, COLOR_END)
+				os.Exit(1)
+			}
+		}
+	}
+
+	// Check if there are branch restrictions
+	if config.AllowedBranches != nil {
+		currentBranchCmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
+		currentBranch, err := currentBranchCmd.Output()
+
+		if err != nil {
+			fmt.Printf("%sError: unable to get current branch: %v\n%s", COLOR_RED, err, COLOR_END)
+			os.Exit(1)
+		}
+
+		if !slices.Contains(config.AllowedBranches, strings.TrimSpace(string(currentBranch))) {
+			fmt.Println(strings.TrimSpace(string(currentBranch)))
+			fmt.Printf("%sError: current branch is not allowed to be tagged\n%s", COLOR_RED, COLOR_END)
+			os.Exit(1)
+		}
+	}
+
+	// Check if Git working directory is clean
+	statusCmd := exec.Command("git", "status", "--porcelain")
+	statusOut, err := statusCmd.Output()
 
 	if err != nil {
-		fmt.Printf("%sError on git tag (maybe tag already exists?): %v\n%s", COLOR_RED, err, COLOR_END)
+		fmt.Printf("%sError executing git command: %v\n%s", COLOR_RED, err, COLOR_END)
 		os.Exit(1)
 	}
 
-	fmt.Printf("%sSuccesfully tagged %s\n%s", COLOR_GREEN, tag, COLOR_END)
+	if len(string(statusOut)) != 0 {
+		fmt.Printf("%sError: Git working directory not clean. Commit your changes before versioning.\n%s", COLOR_RED, COLOR_END)
+		os.Exit(1)
+	}
+
+	// Pull git changes and tags
+	pullCmd := exec.Command("git", "pull", "--tags")
+	_, err = pullCmd.Output()
+
+	if err != nil {
+		fmt.Printf("%sError executing git pull command: %v\n%s", COLOR_RED, err, COLOR_END)
+		os.Exit(1)
+	}
+
+	// Get the tags to create
+
+	tags := make([]string, 0)
+	for _, label := range labels {
+		latestTag := getLatestTag(label)
+
+		tag := incrementTag(label, latestTag, inc, pre, release)
+
+		tags = append(tags, tag)
+	}
+
+	if !yesNoPrompt(fmt.Sprintf("Creating tags: <%s>. Are you sure?", strings.Join(tags, ", "))) {
+		os.Exit(0)
+	}
+
+	// Create tags
+	for _, tag := range tags {
+		tagCmd := exec.Command("git", "tag", "-a", tag, "-m", fmt.Sprintf(`"%s"`, tag))
+		_, err = tagCmd.Output()
+
+		if err != nil {
+			fmt.Printf("%sError on git tag (maybe tag already exists?): %v\n%s", COLOR_RED, err, COLOR_END)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%sSuccesfully tagged %s\n%s", COLOR_GREEN, tag, COLOR_END)
+	}
 
 	// Push tag
 	pushCmd := exec.Command("git", "push", "--tags")
@@ -273,12 +302,12 @@ func increment(label, inc, pre string, release bool, configFile string) {
 		os.Exit(1)
 	}
 
-	fmt.Printf("%sSuccesfully pushed tag %s\n%s", COLOR_GREEN, tag, COLOR_END)
+	fmt.Printf("%sSuccesfully pushed tags\n%s", COLOR_GREEN, COLOR_END)
 }
 
 func main() {
 	// CLI
-	label := flag.String("l", "", "label: sets a prefix label only on the git tag (useful for monorepos to differentiate tags from multiple projects)")
+	labelStr := flag.String("l", "", "label: sets a prefix label only on the git tag (useful for monorepos to differentiate tags from multiple projects)")
 	inc := flag.String("i", "", "increment mode: the increment type. valid inputs: major | minor | patch")
 	pre := flag.String("p", "", "prerelease: sets a prerelease suffix")
 	release := flag.Bool("r", false, "release: removes prerelease suffix. Overrides prerelease option")
@@ -287,5 +316,5 @@ func main() {
 	flag.Parse()
 
 	// Execute
-	increment(*label, *inc, *pre, *release, *configFile)
+	increment(*labelStr, *inc, *pre, *release, *configFile)
 }
